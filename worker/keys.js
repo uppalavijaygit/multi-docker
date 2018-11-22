528c03bd0759b82cdecd018746f09cb49117d855
Vijay Kumar Uppala <vijaykumar.uppala@xchanging.my.local>
2018-11-23 07:31:54 +0800

    Complex_project_first_setup

---
+++ worker/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatebase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPORT: process.env.PGPORT
+};
